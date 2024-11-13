d47e6d2199f4f313892ee179dc5147df095beb12
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-13 18:23:41 +0100

    Initial Commit

---
+++ iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Jessy Viranaiken on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
