665ee27844d1fef1c9f5a489335419c8fc2ef76f
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-18 18:25:31 +0100

    Update: UI/UX

--- Sources/Components/WeatherLocation.swift
+++ Sources/Components/WeatherLocation.swift
@@ -1,39 +1,53 @@
-//
-//  WeatherLocation.swift
-//  iWeather
-//
-//  Created by Jessy Viranaiken on 14/11/2024.
-//
+  //
+  //  WeatherLocation.swift
+  //  iWeather
+  //
+  //  Created by Jessy Viranaiken on 14/11/2024.
+  //
 
 import SwiftUI
 import WeatherKit
+import CoreLocation
 
 struct WeatherLocation: View {
   
-  let weather: Weather?
+  @Environment(LocationController.self) private var locationController
+  @Environment(MapController.self) private var mapController
   
-    var body: some View {
-      if weather != nil {
-        VStack{
-          Text("Soual")
-            .font(.largeTitle)
-            .bold()
-          Text("\(self.weather!.currentWeather.getIconFromCondition())")
-            .font(.system(size: 125))
-            .shadow(color: .black.opacity(0.5), radius: 50)
-          Text("\(String(format: "%.0f", self.weather!.currentWeather.apparentTemperature.value))°")
+  let weather: Weather?
+  let location: CLLocation?
+  @State var placemark: CLPlacemark? = nil
+  
+  var body: some View {
+    VStack{
+      if self.weather != nil {
+        if self.placemark != nil {
+          Text(self.placemark!.locality!)
             .font(.title)
             .bold()
-          Text(self.weather!.currentWeather.condition.description)
-            .font(.title)
         }
-        .frame(width: 375)
+        Text("\(self.weather!.currentWeather.getIconFromCondition())")
+          .font(.system(size: 125))
+          .shadow(color: .black.opacity(0.5), radius: 50)
+        Text("\(String(format: "%.0f", self.weather!.currentWeather.apparentTemperature.value))°")
+          .font(.title2)
+          .bold()
+        Text(self.weather!.currentWeather.condition.description)
+          .font(.title2)
+          .frame(width: 375)
+          .task {
+            if self.location != nil {
+              self.placemark = await mapController.convertToCLPlacemark(self.location!)
+            }
+          }
       } else {
-        
+        ProgressView()
+          .progressViewStyle(CircularProgressViewStyle())
       }
     }
+  }
 }
 
-//#Preview {
-//    WeatherLocation()
-//}
+  //#Preview {
+  //    WeatherLocation()
+  //}
