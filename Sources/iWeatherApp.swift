1bb41b479d9dea42e9014b0a468e04352916f014
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-18 18:43:32 +0100

    Update: Add a new environments variables

--- Sources/iWeatherApp.swift
+++ Sources/iWeatherApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct iWeatherApp: App {
+  
+  @State private var weatherController = WeatherController()
+  @State private var locationController = LocationController()
+  @State private var mapController = MapController()
+  
   var body: some Scene {
     WindowGroup {
       LandingView()
+        .environment(weatherController)
+        .environment(locationController)
+        .environment(mapController)
     }
   }
 }
