dce791b6d05a7e5dcde6f22b7475e0a2f09056cb
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-18 18:39:57 +0100

    Update: func getIconFromCondition and add new func getColor...InVertical

--- Sources/Extensions/CurrentWeather.swift
+++ Sources/Extensions/CurrentWeather.swift
@@ -12,23 +12,40 @@ import SwiftUI
 extension CurrentWeather {
   func getIconFromCondition() -> String {
     switch condition {
-      case .blowingDust: return "⛈️"
-      case .clear, .smoky: return "☀️"
-      case .cloudy, .foggy, .haze: return "☁️"
+      case .clear: return "☀️"
       case .mostlyClear: return "🌤️"
-      case .mostlyCloudy: return "⛅️"
-      case .partlyCloudy: return "🌥️"
-      default: return "?"
+      case .partlyCloudy: return "⛅"
+      case .mostlyCloudy: return "🌥️"
+      case .cloudy, .blizzard: return "☁️"
+      case .windy: return "💨"
+      case .smoky, .haze: return "🌫️"
+      case .drizzle: return "🌦️"
+      case .rain, .heavyRain: return "🌧️"
+      case .snow, .heavySnow: return "🌨️"
+      case .freezingDrizzle, .freezingRain: return "❄️"
+      case .tropicalStorm, .hurricane: return "🌪️"
+      case .hot: return "🔥"
+      default: return "❓"
     }
   }
   
-  func getColorGradientFromTemperature() -> LinearGradient {
+  func getColorGradientFromTemperatureInHorizontal() -> LinearGradient {
     switch temperature.value {
-      case ..<20 : return LinearGradient(colors: [.gray, Color(.systemGray6)], startPoint: .leading, endPoint: .trailing)
-      case 20...30 : return LinearGradient(colors: [.blue, .cyan.opacity(0)], startPoint: .leading, endPoint: .trailing)
+      case ..<10 : return LinearGradient(colors: [.gray, Color(.systemGray6)], startPoint: .leading, endPoint: .trailing)
+      case 10...30 : return LinearGradient(colors: [.blue, .cyan.opacity(0)], startPoint: .leading, endPoint: .trailing)
       case 30...40 : return LinearGradient(colors: [.yellow, .yellow.opacity(1)], startPoint: .leading, endPoint: .trailing)
       case 40... : return LinearGradient(colors: [.red, .red.opacity(0.75)], startPoint: .leading, endPoint: .trailing)
       default: return LinearGradient(colors: [.white, .white], startPoint: .leading, endPoint: .trailing)
+    }
+  }
+  
+  func getColorGradientFromTemperatureInVertical() -> LinearGradient {
+    switch temperature.value {
+      case ..<10 : return LinearGradient(colors: [.gray, Color(.systemGray6)], startPoint: .bottom, endPoint: .top)
+      case 10...30 : return LinearGradient(colors: [.blue, .cyan.opacity(0)], startPoint: .bottom, endPoint: .top)
+      case 30...40 : return LinearGradient(colors: [.yellow, .yellow.opacity(1)], startPoint: .bottom, endPoint: .top)
+      case 40... : return LinearGradient(colors: [.red, .red.opacity(0.75)], startPoint: .bottom, endPoint: .top)
+      default: return LinearGradient(colors: [.white, .white], startPoint: .bottom, endPoint: .top)
     }
   }
 }
