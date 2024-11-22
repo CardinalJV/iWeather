575dfe9580f2d178e0f81688db1b6b9864463d2a
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-22 18:42:12 +0100

    Create: New Extensions file

---
+++ Sources/Extensions/DayWeather.swift
@@ -0,0 +1,28 @@
+//
+//  Forecast.swift
+//  iWeather
+//
+//  Created by Jessy Viranaiken on 22/11/2024.
+//
+
+import Foundation
+import WeatherKit
+
+extension DayWeather {
+  func getIconFromCondition() -> String {
+    switch condition {
+      case .clear: return "sun.max.fill"
+      case .mostlyClear, .partlyCloudy: return "cloud.sun.fill"
+      case .cloudy, .blizzard, .mostlyCloudy: return "cloud.fill"
+      case .windy: return "wind"
+      case .smoky, .haze: return "smoke.fill"
+      case .drizzle: return "cloud.sun.rain.fill"
+      case .rain, .heavyRain: return "cloud.heavyrain.fill"
+      case .snow, .heavySnow: return "cloud.snow.fill"
+      case .freezingDrizzle, .freezingRain: return "snowflake"
+      case .tropicalStorm, .hurricane: return "tornado"
+      case .hot: return "thermometer.high"
+      default: return "❓"
+    }
+  }
+}
--- Sources/Extensions/Forecast.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Forecast.swift
-//  iWeather
-//
-//  Created by Jessy Viranaiken on 22/11/2024.
-//
-
-import Foundation
