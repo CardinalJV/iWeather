d08a4f023c86fe3a728d06cb0816a314a6e15e4b
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-25 18:39:30 +0100

    Update: UI/UX

--- Sources/Components/WeatherForecast.swift
+++ Sources/Components/WeatherForecast.swift
@@ -5,28 +5,36 @@
 //  Created by Jessy Viranaiken on 22/11/2024.
 //
 
-//import SwiftUI
-//import WeatherKit
-//
-//struct WeatherForecast: View {
-//  
-//  let weather: Weather
-//  
-//    var body: some View {
-//      VStack{
-//        LazyHGrid(rows: [GridItem(.flexible(), spacing: 10)]){
-//          ForEach(self.weather.dailyForecast.forecast, id: \.date) { day in
-//            VStack{
-//              Text("\(day.date)")
-//              Image(systemName: day.getIconFromCondition())
-//              Text("\(day.w)")
-//              day.t
-//            }
-//          }
-//        }
-//      }
-//    }
-//}
+import SwiftUI
+import WeatherKit
+
+struct WeatherForecast: View {
+  
+  let weather: Weather
+  
+    var body: some View {
+      VStack{
+        VStack{
+          Text("Forecast weather by hours")
+          Divider()
+          ScrollView(.horizontal){
+            LazyHGrid(rows: [GridItem(.flexible(), spacing: 10)]){
+              ForEach(self.weather.hourlyForecast.forecast.prefix(24), id: \.date) { hour in
+                VStack{
+                  Text("\(hour.getHourFromForecast())")
+                  Spacer()
+                  Image(systemName: hour.getIconFromCondition())
+                    .foregroundStyle(.black)
+                  Spacer()
+                }
+              }
+            }
+          }
+        }
+        .frame(width: 350, height: 175)
+      }
+    }
+}
 
 //#Preview {
 //    WeatherForecast()
---
+++ Sources/Extensions/HourWeather.swift
@@ -0,0 +1,13 @@
+//
+//  HourlyWeather.swift
+//  iWeather
+//
+//  Created by Jessy Viranaiken on 25/11/2024.
+//
+
+import Foundation
+import WeatherKit
+
+extension HourWeather {
+  
+}
