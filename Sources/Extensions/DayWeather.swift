304e382a15587507e6f6efff331c9fa3370d8583
CardinalJV <jessy.viranaiken@icloud.com>
2024-12-04 18:08:53 +0100

    Update: Add new function getRoundedHighTemperature()

--- Sources/Extensions/DayWeather.swift
+++ Sources/Extensions/DayWeather.swift
@@ -9,24 +9,15 @@ import Foundation
 import WeatherKit
 
 extension DayWeather {
-  func getIconFromCondition() -> String {
-    switch condition {
-      case .clear: return "sun.max.fill"
-      case .mostlyClear, .partlyCloudy: return "cloud.sun.fill"
-      case .cloudy, .blizzard, .mostlyCloudy: return "cloud.fill"
-      case .windy: return "wind"
-      case .smoky, .haze: return "smoke.fill"
-      case .drizzle: return "cloud.sun.rain.fill"
-      case .rain, .heavyRain: return "cloud.heavyrain.fill"
-      case .snow, .heavySnow: return "cloud.snow.fill"
-      case .freezingDrizzle, .freezingRain: return "snowflake"
-      case .tropicalStorm, .hurricane, .thunderstorms: return "tornado"
-      case .hot: return "thermometer.high"
-      default: return "❓"
-    }
-  }
-  
   func getDayFromForecast() -> Int {
     return Calendar.current.component(.day, from: self.date)
+  }
+  
+  func getRoundedLowTemperature() -> String {
+    return "\(String(format: "%.0f", self.lowTemperature.value))°"
+  }
+  
+  func getRoundedHighTemperature() -> String {
+    return "\(String(format: "%.0f", self.highTemperature.value))°"
   }
 }
