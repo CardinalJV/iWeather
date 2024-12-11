bf7b6449b2f2cd6af0b5e6ce3cbe1938d0192608
CardinalJV <jessy.viranaiken@icloud.com>
2024-12-11 18:39:44 +0100

    Update: UI/UX

--- Sources/Components/WeatherDataList.swift
+++ Sources/Components/WeatherDataList.swift
@@ -1,13 +1,87 @@
+  //  //
+  //  //  WeatherItems.swift
+  //  //  iWeather
+  //  //
+  //  //  Created by Jessy Viranaiken on 15/11/2024.
+  //  //
   //
-  //  WeatherItems.swift
-  //  iWeather
+  //import SwiftUI
   //
-  //  Created by Jessy Viranaiken on 15/11/2024.
+  //struct WeatherDataList: View {
   //
+  //  @Environment(DataController.self) private var dataController
+  //
+  //  @Namespace private var namespace
+  //
+  //  @State private var showAnimation = false
+  //  @State private var dataRef: [DataModel] = []
+  //
+  //  var body: some View {
+  //    VStack{
+  //      if dataController.datas.isEmpty {
+  //        Text("No data found in Storage")
+  //          .foregroundStyle(.white)
+  //          .font(.body)
+  //          .bold()
+  //      } else {
+  //        VStack{
+  //            //          ForEach(dataController.datas) { data in
+  //            //            NavigationLink {
+  //            //              WeatherView(data: data)
+  //            //                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+  //            //            } label: {
+  //            //              if self.showAnimation {
+  //            //                WeatherDataListItem(data: data)
+  //            //                  .shadow(color: .black.opacity(0.25), radius: 10)
+  //            //              }
+  //            //            }
+  //            //            .transition(.slide)
+  //            //          }
+  //          ForEach(dataRef, id: \.id) { data in
+  //              //            withAnimation(.bouncy.delay(3)) {
+  //            NavigationLink {
+  //              WeatherView(data: data)
+  //                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+  //            } label: {
+  //                //              if self.showAnimation {
+  //              WeatherDataListItem(data: data)
+  //                .shadow(color: .black.opacity(0.25), radius: 10)
+  //                //              }
+  //            }
+  //            .transition(.move(edge: .leading))
+  //              //            }
+  //          }
+  //        }
+  //        .animation(.bouncy.delay(5), value: self.dataRef)
+  //        .onAppear{
+  //          self.dataRef.removeAll()
+  //          for data in dataController.datas {
+  //            if !self.dataRef.contains(data){
+  //                //            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+  ////              withAnimation(.bouncy.delay(3)) {
+  //                self.dataRef.append(data)
+  ////              }
+  //                //            }
+  //            }
+  //          }
+  //        }
+  //          //        .onAppear{
+  //          //          withAnimation(.bouncy.delay(1)){
+  //          //            self.showAnimation = true
+  //          //          }
+  //          //        }
+  //          //        .animation(.bouncy, value: dataController.datas)
+  //      }
+  //    }
+  //    .onAppear{
+  //      dataController.fetchData()
+  //    }
+  //  }
+  //
+  //
+  //}
 
 import SwiftUI
-import WeatherKit
-import CoreLocation
 
 struct WeatherDataList: View {
   
@@ -18,29 +92,37 @@ struct WeatherDataList: View {
   @State private var showAnimation = false
   
   var body: some View {
-    VStack{
+    VStack {
       if dataController.datas.isEmpty {
         Text("No data found in Storage")
           .foregroundStyle(.white)
           .font(.body)
           .bold()
       } else {
-        VStack{
-          ForEach(dataController.datas) { data in
-            NavigationLink {
-              WeatherView(data: data)
-                .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
-            } label: {
-              WeatherDataListItem(data: data)
-                .shadow(color: .black.opacity(0.25), radius: 10)
+        ScrollView{
+          VStack{
+            ForEach(dataController.datas, id: \.id) { data in
+              NavigationLink {
+                WeatherView(data: data)
+                  .navigationTransition(.zoom(sourceID: "zoom", in: self.namespace))
+              } label: {
+                WeatherDataListItem(data: data)
+                  .shadow(color: .black.opacity(0.25), radius: 10)
+              }
+              .scaleEffect(self.showAnimation ? 1 : 0)
+              .opacity(self.showAnimation ? 1 : 0)
+              .animation(.smooth.delay(1), value: self.showAnimation)
             }
-            .transition(.move(edge: .bottom))
+          }
+          .onAppear{
+            self.showAnimation = true
           }
         }
-        .animation(.bouncy, value: dataController.datas)
+        .frame(maxHeight: 350)
+        .scrollClipDisabled()
       }
     }
-    .onAppear{
+    .onAppear {
       dataController.fetchData()
     }
   }
